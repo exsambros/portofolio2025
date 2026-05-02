3308546826daef64ebd79d2e23964a818f7fe7ab
Xsam <tudepurnaditya@gmail.com>
2026-05-02 13:33:15 +0800

    uppdate tester
    animasikan lebih smoth

--- portofolio/script.js
+++ portofolio/script.js
@@ -1,3 +1,8 @@
-function sayHello() {
-  alert("Hello, World!");
-}
+// Menginisialisasi animasi dari AOS (Animate On Scroll)
+document.addEventListener("DOMContentLoaded", function() {
+  AOS.init({
+    duration: 1000, // Durasi animasi (dalam milidetik)
+    once: true,     // Animasi hanya berjalan sekali saat di-scroll ke bawah
+    offset: 50      // Jarak offset untuk memicu animasi
+  });
+});
